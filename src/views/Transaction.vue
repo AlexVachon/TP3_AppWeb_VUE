b9de3d6e3017097643b0a5bcb2bbd385145f34d5
Alexandre Vachon <alvac002@edu.cegepgarneau.ca>
2023-12-02 10:36:03 -0500

    template fini

--- src/views/Transaction.vue
+++ src/views/Transaction.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="my-5 m-auto">
+    <div class="my-5 m-auto shadow">
         <div class="card">
             <div class="card-header">
                 <div class="d-flex justify-content-between align-items-center">
@@ -21,17 +21,52 @@
             <div class="card-body">
                 <h1 class="card-title">Historique des factures</h1>
                 <div>
-                    <table class="table m-auto">
-                        <thead>
+                    <div v-if="factures.length === 0">
+                        <p class="badge bg-dark-subtle">Votre historique est vide</p>
+                    </div>
+                    <div v-else>
+                        <table class="table table-hover m-auto shadow-sm" style="width:95%;">
+                            <thead class="table-light">
+                                <tr>
+                                    <th class="col-9">Date</th>
+                                    <th class="col-3">Montant</th>
+                                </tr>
+                            </thead>
+                            <tbody class="overflow-auto">
+                                <tr v-for="(facture) in factures">
+                                    <td>{{ facture.createdAt }}</td>
+                                    <td>{{ facture.price }}</td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="my-5 m-auto shadow">
+        <div class="card">
+            <div class="card-header">
+                Historique des déplacements
+            </div>
+            <div class="card-body">
+                <div v-if="historique.length === 0">
+                    <p class="badge bg-dark-subtle">Votre historique est vide</p>
+                </div>
+                <div v-else>
+                    <table class="table table-hover m-auto shadow-sm" style="width: 95%;">
+                        <thead class="table-light">
                             <tr>
-                                <th>Date</th>
-                                <th>Montant</th>
+                                <td class="col-8">Date</td>
+                                <td class="col-2">Prix</td>
+                                <td class="col-2">Payé</td>
                             </tr>
                         </thead>
                         <tbody class="overflow-auto">
-                            <tr v-for="(facture) in factures">
+                            <tr v-for="(facture) in historique">
                                 <td>{{ facture.createdAt }}</td>
                                 <td>{{ facture.price }}</td>
+                                <td>{{ facture.isPaid }}</td>
                             </tr>
                         </tbody>
                     </table>
@@ -44,15 +79,37 @@
 export default {
     data() {
         return {
-            factures: []
+            factures: [],
+            historique: []
         }
     },
     mounted() {
-        this.fetchFactures()
+        this.fetchFactures(),
+            this.fetchHistoriqueDeplacements()
     },
     methods: {
         fetchFactures() {
-            console.log(`Bearer ${localStorage.getItem('jwt')}`)
+            fetch(`https://api-garenoticket-604fa7d27199.herokuapp.com/facture`, {
+                method: "GET",
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Authorization': `Bearer ${localStorage.getItem('jwt')}`,
+                },
+            })
+                .then(response => {
+                    if (response.ok) {
+                        return response.json()
+                    } else {
+                        throw new Error("Erreur lors de la récupération de l'historique des factures.")
+                    }
+                })
+                .then(data => {
+                    console.log(data)
+                    this.factures = data
+                })
+                .catch(error => console.error(error))
+        },
+        fetchHistoriqueDeplacements() {
             fetch(`https://api-garenoticket-604fa7d27199.herokuapp.com/historique`, {
                 method: "GET",
                 headers: {
@@ -61,17 +118,17 @@ export default {
                 },
             })
                 .then(response => {
-                    if(response.ok){
+                    if (response.ok) {
                         return response.json()
-                    }else{
-                        throw new Error("Erreur lors de la récupération de l'historique des factures.")
+                    } else {
+                        throw new Error("Erreur lors de la récupération de l'historique des déplacements.")
                     }
                 })
                 .then(data => {
                     console.log(data)
-                    this.factures = data
+                    this.historique = data
                 })
-                .catch(error=>console.error(error))
+                .catch(error => console.error(error))
         }
     }
 }
